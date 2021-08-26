0a7f87a358180bbd61cc0bf2e4fe28405b2bf8b9
Alejandro.Castillo <alejandro.castillo@fluxit.com.ar>
2021-08-26 16:09:54 -0300

    total amount cell

---
+++ MyLib/Classes/ProductList/Cells/TotalAmount/TotalAmountTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  TotalAmountTableViewCell.swift
+//  MyLib
+//
+//  Created by Alejandro Castillo on 26/08/2021.
+//
+
+import UIKit
+
+class TotalAmountTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var totalPriceLabel: UILabel?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
